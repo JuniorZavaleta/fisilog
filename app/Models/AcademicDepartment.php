ef0e61878ddd1f2dd4bb691b632671547a9a386c
unknown <takesansw12@gmail.com>
2015-11-11 22:43:25 -0500

    Modelo Departamento Académico

--- app/Models/AcademicDepartment.php
+++ app/Models/AcademicDepartment.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace FisiLog\Models;
-
+use FisiLog\Models\Professor;
 use Illuminate\Database\Eloquent\Model;
 
 class AcademicDepartment extends Model
 {
     protected $table = 'academic_departments';
+
+    public function professor(){
+        return $this->hasMany(Profesor::class,'id');
+    } 
 }
