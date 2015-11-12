628630f676ba49b89c25f2acb5db557d2180ac71
unknown <takesansw12@gmail.com>
2015-11-11 22:39:16 -0500

    Modelo Docente

--- app/Models/Professor.php
+++ app/Models/Professor.php
@@ -1,11 +1,20 @@
 <?php
 
 namespace FisiLog\Models;
-
+use FisiLog\Models\Clase;
+use FisiLog\Models\AcademicDepartament;
 use Illuminate\Database\Eloquent\Model;
 use FisiLog\Models\User;
 
 class Professor extends User
 {
     protected $table ='professors';
+
+    public function academicDepartament(){
+        return $this->belongsTo(AcademicDepartament::class);
+    } 
+
+    public function clase(){
+        return $this->hasMany(Clase::class,'id');
+  	}
 }
