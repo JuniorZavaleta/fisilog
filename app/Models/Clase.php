348a367854e2ec40fb723fb3290a7f33380e930b
unknown <takesansw12@gmail.com>
2015-11-11 22:35:20 -0500

    Modelo Clase

--- app/Models/Clase.php
+++ app/Models/Clase.php
@@ -1,10 +1,19 @@
 <?php
 
 namespace FisiLog\Models;
-
+use FisiLog\Models\Group;
+use FisiLog\Models\Professor;
 use Illuminate\Database\Eloquent\Model;
 
 class Clase extends Model
 {
     protected $table = 'classes';
+
+    public function group(){
+        return $this->belongsTo(Group::class);
+    } 
+
+    public function professor(){
+        return $this->belongsTo(Professor::class);
+    } 
 }
