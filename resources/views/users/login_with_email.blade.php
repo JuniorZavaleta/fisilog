8a9c60e9bd21d0edc2e2082bb2b4fe7b9769479e
unknown <takesansw12@gmail.com>
2015-11-21 11:57:10 -0500

    vista logins

--- resources/views/users/login_with_email.blade.php
+++ resources/views/users/login_with_email.blade.php
@@ -5,7 +5,7 @@
      <div class="row">
       <div class="col-md-7 col-md-offset-2">
        <div class="panel panel-default">
-         <div class="panel-heading">Login</div>
+         <div class="panel-heading">Iniciar Sesión</div>
          <div class="panel-body">
           {!! Form::open(array('route'=>'user.register.process', 'class'=>'form-horizontal', 'method'=>'POST')) !!}
 
