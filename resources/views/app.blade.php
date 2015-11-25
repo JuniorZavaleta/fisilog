84806c58ad21e3c58bf88bde3c931a0544585776
Junior Claudio <junior.claudio@tektonlabs.com>
2015-11-25 15:12:33 -0500

    tabs

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,64 +6,58 @@
     <!-- Scripts -->
     {!! Html::script('js/jquery-1.11.3.min.js') !!}
     {!! Html::script('js/bootstrap.min.js') !!}
-  	<style type="text/css">
-  	.container {
+    <style type="text/css">
+    .container {
     padding: 10px;
      }
     </style>
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-    	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-     	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 </head>
 <body>
     <nav class="navbar navbar-default">
         <div class="container-fluid">
-		    <div class="navbar-header">
-		        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-			    <span class="sr-only">Toggle Navigation</span>
-			    <span class="icon-bar"></span>
-			    <span class="icon-bar"></span>
-			    <span class="icon-bar"></span>
-			</button>
-			<a class="navbar-brand" href="#">FisiLog</a>
-		    </div>
-		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-		        <ul class="nav navbar-nav">
-			    	<li><a href="/">Home</a></li>
-				</ul>
-				<ul class="nav navbar-nav navbar-right">
-				    
-				    @if (Auth::guest())
-				        <li><a href="#">Iniciar Sesión</a></li>
-						<li><a href="{{route('user.register.index')}}">Registrarse</a></li>
-				    @else
-		                <li>
-		                    <a href="#">{{ Auth::user()->name }}</a>
-		                </li>
-		                <li><a href="#">Cerrar Sesión</a></li>
-		                
-			        @endif
-				   
-			        
-				</ul>
-			</div>
-		</div>
-	</nav>
-    <div class="container">
-               @if (Session::has('errors'))
-		    <div class="alert alert-warning" role="alert">
-			<ul>
-	            <strong>Oops! Something went wrong : </strong>
-			    @foreach ($errors->all() as $error)
-				<li>{{ $error }}</li>
-		            @endforeach
-		        </ul>
-		    </div>
-		@endif
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle Navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">FisiLog</a>
+        </div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+            <li><a href="/">Home</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+        @if (Auth::guest())
+          <li><a href="#">Iniciar Sesión</a></li>
+          <li><a href="{{route('user.register.index')}}">Registrarse</a></li>
+        @else
+          <li><a href="#">{{ Auth::user()->name }}</a></li>
+          <li><a href="#">Cerrar Sesión</a></li>
+        @endif
+        </ul>
+      </div>
     </div>
+  </nav>
+    @if (Session::has('errors'))
+      <div class="container">
+        <div class="alert alert-warning" role="alert">
+          <ul>
+            <strong>Oops! Something went wrong : </strong>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+          </ul>
+        </div>
+      </div>
+    @endif
     <input type="hidden" value="{{URL::to('/')}}" id="base_url">
     @yield('content')
 </body>
