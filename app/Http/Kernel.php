92bd773e212a65e75b9310b83db2588c69ebbc1d
Junior Claudio <juniorclaudiozavaleta@gmail.com>
2016-05-29 16:42:10 -0500

    update laravel 5.2

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,6 +21,27 @@ class Kernel extends HttpKernel
     ];
 
     /**
+     * The application's route middleware groups.
+     *
+     * @var array
+     */
+    protected $middlewareGroups = [
+        'web' => [
+            \FisiLog\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \FisiLog\Http\Middleware\VerifyCsrfToken::class,
+        ],
+        'auth' => [
+            \FisiLog\Http\Middleware\Authenticate::class,
+        ],
+        'api' => [
+            'throttle:60,1',
+        ],
+    ];
+
+    /**
      * The application's route middleware.
      *
      * @var array
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,29 +10,32 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+Route::group(['middleware' => ['web', 'auth']], function () {
+   Route::get('users/{id}/edit/register_document', ['as' => 'user.register.document', 'uses' => 'DocumentController@index']);
+   Route::post('users/{id}/edit/register_document', ['as' => 'user.register.document', 'uses' => 'DocumentController@process']);
 
-Route::get('/', function () {
-    return redirect()->route('auth.login');
+   Route::get('/verify_attendance/index',['as'=>'verify_attendance.professor', 'uses'=>'VerifyAttendanceController@index']);
+   Route::get('/verify_attendance/professor',['as'=>'verify_attendance.professor', 'uses'=>'VerifyAttendanceController@getProfessor']);
+
+   Route::get('/attendance/index',['as'=>'attendance.index','uses'=>'AttendanceController@index']);
+   Route::get('/attendance/{clase_id}/student',['as'=>'attendance.student.index','uses'=>'AttendanceController@getStudent']);
+   Route::post('/attendance/{clase_id}/student',['as'=>'attendance.student.preprocess','uses'=>'AttendanceController@findStudent']);
+   Route::post('/attendance/{clase_id}/student_verified',['as'=>'attendance.student.process','uses'=>'AttendanceController@postStudent']);
+   Route::get('/attendance/professor',['as'=>'attendance.professor','uses'=>'AttendanceController@getProfessor']);
 });
-Route::get('/users/register',['as'=>'user.register.index','uses'=>'UserRegisterController@index']);
-Route::post('/users/register',['as'=>'user.register.process','uses'=>'UserRegisterController@process']);
 
-Route::get('users/{id}/edit/register_document', ['as' => 'user.register.document', 'uses' => 'DocumentController@index']);
-Route::post('users/{id}/edit/register_document', ['as' => 'user.register.document', 'uses' => 'DocumentController@process']);
+Route::group(['middleware' => ['web']], function () {
+   Route::get('index', ['as' => 'index', 'uses' => 'IndexController@index']);
+   Route::get('/', function () {
+       return redirect()->route('auth.login');
+   });
+   Route::get('/users/register',['as'=>'user.register.index','uses'=>'UserRegisterController@index']);
+   Route::post('/users/register',['as'=>'user.register.process','uses'=>'UserRegisterController@process']);
 
-Route::get('/verify_attendance/index',['as'=>'verify_attendance.professor', 'uses'=>'VerifyAttendanceController@index']);
-Route::get('/verify_attendance/professor',['as'=>'verify_attendance.professor', 'uses'=>'VerifyAttendanceController@getProfessor']);
 
-Route::get('/attendance/index',['as'=>'attendance.index','uses'=>'AttendanceController@index']);
-Route::get('/attendance/{clase_id}/student',['as'=>'attendance.student.index','uses'=>'AttendanceController@getStudent']);
-Route::post('/attendance/{clase_id}/student',['as'=>'attendance.student.preprocess','uses'=>'AttendanceController@findStudent']);
-Route::post('/attendance/{clase_id}/student_verified',['as'=>'attendance.student.process','uses'=>'AttendanceController@postStudent']);
-Route::get('/attendance/professor',['as'=>'attendance.professor','uses'=>'AttendanceController@getProfessor']);
+   Route::get('login', ['as' => 'auth.login' , 'uses' => 'Auth\AuthController@getLogin']);
+   Route::post('login', ['as' => 'auth.login' , 'uses' => 'Auth\AuthController@postLogin']);
 
-Route::get('index', ['as' => 'index', 'uses' => 'IndexController@index']);
-
-Route::get('login', ['as' => 'auth.login' , 'uses' => 'Auth\AuthController@getLogin']);
-Route::post('login', ['as' => 'auth.login' , 'uses' => 'Auth\AuthController@postLogin']);
-
-Route::get('logout', ['as' => 'auth.logout' , 'uses' => 'Auth\AuthController@logout']);
+   Route::get('logout', ['as' => 'auth.logout' , 'uses' => 'Auth\AuthController@logout']);
+});
 
