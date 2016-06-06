74581bdb7ecdfa7a3fca022560aa9b347b71c2bf
Junior Claudio <juniorclaudiozavaleta@gmail.com>
2016-06-06 00:52:42 -0500

    business classes with new approach

--- app/BusinessClasses/Professor.php
+++ app/BusinessClasses/Professor.php
@@ -1,9 +1,15 @@
 <?php
 namespace FisiLog\BusinessClasses;
+
 use FisiLog\BusinessClasses\AcademicDepartment;
-class Professor extends User {
-   private $academicDepartment;
-   private $professor_type;
+
+use Illuminate\Contracts\Support\Arrayable;
+
+class Professor extends User implements Arrayable {
+
+   protected $academic_department;
+
+   protected $professor_type;
 
    public static $nombrado_type   = 1;
    public static $contratado_type = 2;
@@ -13,16 +19,64 @@ class Professor extends User {
    public static $contratado_type_name = "Contratado";
    public static $auxiliar_type_name   = "Auxiliar";
 
-   public function setAcademicDepartment(AcademicDepartment $academicDepartment) {
+   public function __construct(User $user, $academic_department, $type)
+   {
+      $this->id = $user->getId();
+      $this->name = $user->getName();
+      $this->last_name = $user->getLastName();
+      $this->email = $user->getEmail();
+      $this->password = $user->getPassword();
+      $this->phone = $user->getPhone();
+      $this->user_type = $user->getUserType();
+      $this->photo_url = $user->getPhotoUrl();
+      $this->notification_channel = $user->getNotificationChannel();
+      $this->setAcademicDepartment($academic_department);
+      $this->setProfessorType($type);
+   }
+
+   public function setAcademicDepartment(AcademicDepartment $academicDepartment)
+   {
       $this->academicDepartment = $academicDepartment;
    }
-   public function getAcademicDepartment(){
+
+   public function getAcademicDepartment()
+   {
       return $this->academicDepartment;
    }
-   public function setProfessorType($professor_type) {
+
+   public function setProfessorType($professor_type)
+   {
       $this->professor_type = $professor_type;
    }
-   public function getProfessorType() {
+
+   public function getProfessorType()
+   {
       return $this->professor_type;
+   }
+
+   /**
+     * Get the instance as an array.
+     *
+     * @return array
+     */
+   public function toArray()
+   {
+      return [
+         'id' => $this->id,
+         'name' => $this->name,
+         'last_name' => $this->last_name,
+         'email' => $this->email,
+         'password' => $this->password,
+         'phone' => $this->phone,
+         'notification_channel_id' => $this->notification_channel->getId(),
+         'notification_channel' => $this->notification_channel->toArray(),
+         'photo_url' => $this->photo_url,
+         'user_type_id' => $this->user_type->getId(),
+         'user_type' => $this->user_type->toArray(),
+         'school_id' => $this->academic_department->getId(),
+         'academic_department_id' => $this->academic_department->getId(),
+         'academic_department' => $this->academic_department->toArray(),
+         'professor_type' => $this->professor_type,
+      ];
    }
 }
