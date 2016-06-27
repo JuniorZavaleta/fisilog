298a5dfc70f24f64fbb782247e4eb2cc3831f225
Takeshi Farro <takeshi.farro@gmail.com>
2016-06-26 22:18:18 -0500

    fix session class seed

--- database/seeds/SessionClassSeeder.php
+++ database/seeds/SessionClassSeeder.php
@@ -13,14 +13,14 @@ class SessionClassSeeder extends Seeder
    {
       DB::table('sessions_class')->truncate();
       DB::table('sessions_class')->insert([
-         ['id'=>1, 'class_id'=>1, 'session_date'=>date('Y-m-d',strtotime('05/06/2016')), 'status'=>'I'],
-         ['id'=>2, 'class_id'=>1, 'session_date'=>date('Y-m-d',strtotime('12/06/2016')), 'status'=>'C'],
-         ['id'=>3, 'class_id'=>1, 'session_date'=>date('Y-m-d',strtotime('19/06/2016')), 'status'=>'I'],
-         ['id'=>4, 'class_id'=>1, 'session_date'=>date('Y-m-d',strtotime('26/06/2016')), 'status'=>'I'],
-         ['id'=>5, 'class_id'=>2, 'session_date'=>date('Y-m-d',strtotime('05/06/2016')), 'status'=>'I'],
-         ['id'=>6, 'class_id'=>2, 'session_date'=>date('Y-m-d',strtotime('12/06/2016')), 'status'=>'C'],
-         ['id'=>7, 'class_id'=>2, 'session_date'=>date('Y-m-d',strtotime('19/06/2016')), 'status'=>'I'],
-         ['id'=>8, 'class_id'=>2, 'session_date'=>date('Y-m-d',strtotime('26/06/2016')), 'status'=>'I'],
+         ['id'=>1, 'class_id'=>1, 'session_date'=>date('Y-m-d',strtotime('06/05/2016')), 'status'=>'I'],
+         ['id'=>2, 'class_id'=>1, 'session_date'=>date('Y-m-d',strtotime('06/12/2016')), 'status'=>'C'],
+         ['id'=>3, 'class_id'=>1, 'session_date'=>date('Y-m-d',strtotime('06/19/2016')), 'status'=>'I'],
+         ['id'=>4, 'class_id'=>1, 'session_date'=>date('Y-m-d',strtotime('06/26/2016')), 'status'=>'I'],
+         ['id'=>5, 'class_id'=>2, 'session_date'=>date('Y-m-d',strtotime('06/05/2016')), 'status'=>'I'],
+         ['id'=>6, 'class_id'=>2, 'session_date'=>date('Y-m-d',strtotime('06/12/2016')), 'status'=>'C'],
+         ['id'=>7, 'class_id'=>2, 'session_date'=>date('Y-m-d',strtotime('06/19/2016')), 'status'=>'I'],
+         ['id'=>8, 'class_id'=>2, 'session_date'=>date('Y-m-d',strtotime('06/26/2016')), 'status'=>'I'],
       ]);
    }
 }
