94671dd7592a51421f796646a70372b85940899e
Takeshi Farro <takeshi.farro@gmail.com>
2016-07-07 12:57:51 -0500

    seeders of users, session and studentsxgroups

--- database/seeds/StudentXGroupSeeder.php
+++ database/seeds/StudentXGroupSeeder.php
@@ -9,6 +9,12 @@ class StudentXGroupSeeder extends Seeder
       DB::table('students_x_groups')->truncate();
       DB::table('students_x_groups')->insert([
          ['id'=>1, 'student_id'=> 1, 'group_id'=>1],
+         ['id'=>2, 'student_id'=> 3, 'group_id'=>1],
+         ['id'=>3, 'student_id'=> 5, 'group_id'=>1],
+         ['id'=>4, 'student_id'=> 7, 'group_id'=>1],
+         ['id'=>5, 'student_id'=> 8, 'group_id'=>1],
+         ['id'=>6, 'student_id'=> 9, 'group_id'=>1],
+         ['id'=>7, 'student_id'=> 10, 'group_id'=>1],
       ]);
    }
 }
