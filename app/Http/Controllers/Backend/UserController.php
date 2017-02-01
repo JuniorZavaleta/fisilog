95bc92d6a54b7c51a8334e1f20023da24c72daaa
Junior Claudio <juniorclaudiozavaleta@gmail.com>
2017-01-31 20:34:05 -0500

    remove dao layer of user create and index

--- app/Http/Controllers/Backend/UserController.php
+++ app/Http/Controllers/Backend/UserController.php
@@ -5,51 +5,35 @@ use Illuminate\Http\Request;
 
 use FisiLog\Http\Controllers\Controller;
 
-use FisiLog\DAO\DaoEloquentFactory;
-
 use FisiLog\Http\Requests\Backend\User\StoreRequest;
 
-use FisiLog\BusinessClasses\User;
-use FisiLog\BusinessClasses\Student;
-use FisiLog\BusinessClasses\Professor;
-use FisiLog\BusinessClasses\Document;
+use FisiLog\Models\User;
+use FisiLog\Models\UserType;
+use FisiLog\Models\DocumentType;
+use FisiLog\Models\School;
+use FisiLog\Models\AcademicDepartment;
 
 class UserController extends Controller
 {
-   public function __construct(DaoEloquentFactory $dao)
+   public function __construct()
    {
-      $this->document_persistence            = $dao->getDocumentDAO();
-      $this->document_type_persistence       = $dao->getDocumentTypeDAO();
-      $this->school_persistence              = $dao->getSchoolDAO();
-      $this->academic_dep_persistence        = $dao->getAcademicDepartmentDAO();
-      $this->notification_channel_persistence = $dao->getNotificationChannelDAO();
-      $this->user_type_persistence           = $dao->getUserTypeDAO();
-
-      $this->user_persistence                = $dao->getUserDAO();
-      $this->student_persistence             = $dao->getStudentDAO();
-      $this->professor_persistence           = $dao->getProfessorDAO();
-
       $this->notification_by_email_id        = 2;
    }
 
    public function index()
    {
-      $users = $this->user_persistence->paginate();
+      $users = User::paginate(10);
 
-      $data = [
-         'users' => $users,
-      ];
-
-      return view('backend.users.index', $data);
+      return view('backend.users.index', compact('users'));
    }
 
    public function create()
    {
-      $document_types       = $this->document_type_persistence->getAll();
-      $schools              = $this->school_persistence->getAll();
-      $academic_departments = $this->academic_dep_persistence->getAll();
+      $document_types       = DocumentType::all();
+      $schools              = School::all();
+      $academic_departments = AcademicDepartment::all();
       $professor_types      = config('enums.professor_types');
-      $user_types           = $this->user_type_persistence->getAll();
+      $user_types           = UserType::all();
 
       $data = [
          'document_types'       => $document_types,
--- resources/views/backend/users/create.blade.php
+++ resources/views/backend/users/create.blade.php
@@ -3,9 +3,7 @@
 @section('content')
 <title>Registro de Usuario</title>
 <style type="text/css">
-   .form-horizontal .control-label{
-    text-align: left;
-   }
+.form-horizontal .control-label{ text-align: left; }
 </style>
 <script src="{{ url('js/users/register/main.js') }}"></script>
 <script type="text/javascript">
@@ -13,255 +11,225 @@
 </script>
 
 <div class="row">
-   <div class="col-xs-12">
-      <h2>Registrar usuario</h2>
-   </div>
+  <div class="col-xs-12">
+    <h2>Registrar usuario</h2>
+  </div>
 </div>
 
 @include('backend.show_errors')
 
 <div class="row">
-   <form class="form-horizontal" action="{{ route('users.store') }}" method="POST" enctype="multipart/form-data">
-      {!! csrf_field() !!}
-      <div class="col-sm-6">
-         <!-- Start container -->
-         <div class="form-container">
-            <!-- Start Nombres -->
-            <div class="form-group">
+  <form class="form-horizontal" action="{{ route('users.store') }}" method="POST" enctype="multipart/form-data">
+    {!! csrf_field() !!}
+    <div class="col-sm-6">
+      <!-- Start container -->
+      <div class="form-container">
+        <!-- Start Nombres -->
+        <div class="form-group">
+          <div class="col-sm-4">
+            <label class="control-label">Nombre</label>
+          </div>
 
-               <div class="col-sm-4">
-                  <label class="control-label">Nombre</label>
-               </div>
+          <div class="col-sm-8">
+            <input type="text" class="form-control" name="name" placeholder="Ingrese sus nombres" value="{{ old('name') }}">
+          </div>
+        </div>
+        <!-- End Nombres -->
 
-               <div class="col-sm-8">
-                  <input type="text" class="form-control" name="name" placeholder="Ingrese sus nombres" value="{{ old('name') }}">
-               </div>
+        <!-- Start Apellidos -->
+        <div class="form-group">
+          <div class="col-sm-4">
+            <label class="control-label">Apellidos</label>
+          </div>
 
-            </div>
-            <!-- End Nombres -->
+          <div class="col-sm-8">
+            <input type="text" class="form-control" name="lastname" placeholder="Ingrese sus apellidos" value="{{ old('lastname') }}">
+          </div>
 
-            <!-- Start Apellidos -->
-            <div class="form-group">
+        </div>
+        <!-- End Apellidos -->
 
-               <div class="col-sm-4">
-                  <label class="control-label">Apellidos</label>
-               </div>
+        <!-- Start Tipo Usuario -->
+        <div class="form-group">
+          <div class="col-sm-4">
+            <label class="control-label">Tipo Usuario</label>
+          </div>
 
-               <div class="col-sm-8">
-                  <input type="text" class="form-control" name="lastname" placeholder="Ingrese sus apellidos" value="{{ old('lastname') }}">
-               </div>
+          <div class="col-sm-8">
+            <select id="user_type" name="user_type" class="form-control">
+              <option value="0" >Seleccione el tipo de Usuario</option>
+              @foreach ($user_types as $user_type)
+              <option value="{{ $user_type->id }}" {{ old('user_type') == $user_type->id ? 'selected' : '' }} >{{ $user_type->name }}</option>
+              @endforeach
+            </select>
+          </div>
+        </div>
+        <!-- End Tipo Usuario -->
 
-            </div>
-            <!-- End Apellidos -->
+        <!-- Start Tipo de documento -->
+        <div class="form-group">
+          <div class="col-sm-4">
+            <label class="control-label">Tipo de documento</label>
+          </div>
 
-            <!-- Start Tipo Usuario -->
-            <div class="form-group">
+          <div class="col-sm-8">
+            <select name="document_type" class="form-control">
+              <option value="0" >Seleccione un tipo de Documento</option>
+              @foreach($document_types as $document_type)
+              <option value="{{$document_type->id}}" {{ old('document_type') == $document_type->id  ? 'selected' : '' }} >{{$document_type->name}}</option>
+              @endforeach
+            </select>
+          </div>
+        </div>
+        <!-- End Tipo de documento -->
 
-               <div class="col-sm-4">
-                  <label class="control-label">Tipo Usuario</label>
-               </div>
+        <!-- Start Numero de documento -->
+        <div class="form-group">
+          <div class="col-sm-4">
+            <label class="control-label">Número de Documento</label>
+          </div>
 
-               <div class="col-sm-8">
-                  <select id="user_type" name="user_type" class="form-control">
-                     <option value="0" >Seleccione el tipo de Usuario</option>
-                     @foreach($user_types as $user_type)
-                        <option value="{{ $user_type->getId() }}" {{ old('user_type') == $user_type->getId() ? 'selected' : '' }} >{{ $user_type->getRealName() }}</option>
-                     @endforeach
-                  </select>
-               </div>
+          <div class="col-sm-8">
+            <input type="text" class="form-control" name="document_code" placeholder="Ingrese el número de documento" value="{{ old('document_code') }}">
+          </div>
+        </div>
+        <!-- End Numero de documento -->
 
-            </div>
-            <!-- End Tipo Usuario -->
+        <!-- Start Telefono -->
+        <div class="form-group">
+          <div class="col-sm-4">
+            <label class="control-label">Telefono</label>
+          </div>
 
-            <!-- Start Tipo de documento -->
-            <div class="form-group">
+          <div class="col-sm-8">
+            <input type="text" class="form-control" name="phone" placeholder="Ingrese su telefono" value="{{ old('phone') }}">
+          </div>
+        </div>
+        <!-- End telefono -->
 
-               <div class="col-sm-4">
-                  <label class="control-label">Tipo de documento</label>
-               </div>
+        <!-- Start email -->
+        <div class="form-group">
+          <div class="col-sm-4">
+            <label class="control-label">E-mail</label>
+          </div>
 
-               <div class="col-sm-8">
-                  <select name="document_type" class="form-control">
-                     <option value="0" >Seleccione un tipo de Documento</option>
-                  @foreach($document_types as $document_type)
-                     <option value="{{$document_type->getId()}}" {{ old('document_type') == $document_type->getId()  ? 'selected' : '' }} >{{$document_type->getName()}}</option>
-                  @endforeach
-                  </select>
-               </div>
+          <div class="col-sm-8">
+            <input type="text" name="email" class="form-control" placeholder="Ingrese su e-mail" value="{{ old('email') }}">
+          </div>
+        </div>
+        <!-- End email -->
 
-            </div>
-            <!-- End Tipo de documento -->
+        <!-- Start password -->
+        <div class="form-group">
+          <div class="col-sm-4">
+            <label class="control-label">Password</label>
+          </div>
 
-            <!-- Start Numero de documento -->
-            <div class="form-group">
-
-               <div class="col-sm-4">
-                  <label class="control-label">Número de Documento</label>
-               </div>
-
-               <div class="col-sm-8">
-                  <input type="text" class="form-control" name="document_code" placeholder="Ingrese el número de documento" value="{{ old('document_code') }}">
-               </div>
-
-            </div>
-            <!-- End Numero de documento -->
-
-            <!-- Start Telefono -->
-            <div class="form-group">
-
-               <div class="col-sm-4">
-                  <label class="control-label">Telefono</label>
-               </div>
-
-               <div class="col-sm-8">
-                  <input type="text" class="form-control" name="phone" placeholder="Ingrese su telefono" value="{{ old('phone') }}">
-               </div>
-
-            </div>
-            <!-- End telefono -->
-
-            <!-- Start email -->
-            <div class="form-group">
-
-               <div class="col-sm-4">
-                 <label class="control-label">E-mail</label>
-               </div>
-
-               <div class="col-sm-8">
-                  <input type="text" name="email" class="form-control" placeholder="Ingrese su e-mail" value="{{ old('email') }}">
-               </div>
-
-            </div>
-            <!-- End email -->
-
-            <!-- Start password -->
-            <div class="form-group">
-
-               <div class="col-sm-4">
-                 <label class="control-label">Password</label>
-               </div>
-
-               <div class="col-sm-8">
-                  <input type="password" name="password" class="form-control" placeholder="Ingrese su password" value="">
-               </div>
-
-            </div>
-            <!-- End password -->
-         </div>
+          <div class="col-sm-8">
+            <input type="password" name="password" class="form-control" placeholder="Ingrese su password" value="">
+          </div>
+        </div>
+        <!-- End password -->
       </div>
-      <!-- End primera columna -->
+    </div>
+    <!-- End primera columna -->
 
-      <!-- Start segunda columna -->
-      <div class="col-sm-6">
-         <!-- Start Foto -->
-         <div class="form-group">
+    <!-- Start segunda columna -->
+    <div class="col-sm-6">
+      <!-- Start Foto -->
+      <div class="form-group">
+        <div class="col-sm-3">
+          <label class="control-label">Foto</label>
+        </div>
 
-            <div class="col-sm-3">
-               <label class="control-label">Foto</label>
-            </div>
-
-            <div class="col-sm-9">
-                <input type="file" name="photo" style="width: 100%;">
-                   {{-- {!! Form::file('photo', ['style'=>'width:100%']) !!} --}}
-            </div>
-
-         </div>
-         <!-- End Foto -->
-
-         <!-- Start EAP -->
-         <div class="form-group student-field" hidden>
-
-            <div class="col-sm-3">
-               <label class="control-label">EAP</label>
-            </div>
-
-            <div class="col-sm-9">
-               <select name="school_id" class="form-control">
-                  <option value="0" selected>Seleccione la EAP del alumno</option>
-               @foreach($schools as $school)
-                   <option value="{{ $school->getId() }}">{{ $school->getName() }}</option>
-               @endforeach
-               </select>
-            </div>
-
-         </div>
-         <!-- End EAP -->
-
-         <!-- Start Codigo -->
-         <div class="form-group student-field" hidden>
-
-            <div class="col-sm-3">
-               <label class="control-label">Código</label>
-            </div>
-
-            <div class="col-sm-9">
-               <input type="text" name="student_code" class="form-control" placeholder="Ingrese el código de alumno del estudiante" value="{{ old('student_code') }}">
-            </div>
-
-         </div>
-         <!-- End Codigo -->
-
-         <!-- Start año de ingreso -->
-         <div class="form-group student-field" hidden>
-
-            <div class="col-sm-3">
-               <label class="control-label">Año de ingreso</label>
-            </div>
-
-            <div class="col-sm-9">
-               <input type="text" name="year_of_entry" class="form-control" placeholder="Ingrese el año de ingreso del estudiante" value="{{ old('year_of_entry') }}">
-            </div>
-
-         </div>
-         <!-- End año de ingreso -->
-
-         <!-- Start tipo de profesor -->
-         <div class="form-group professor-field" hidden>
-
-            <div class="col-sm-3">
-               <label class="control-label">Tipo de profesor</label>
-            </div>
-
-            <div class="col-sm-9">
-               <select name="professor_type" class="form-control">
-                  <option value="0" selected>Seleccione el tipo de profesor</option>
-               @foreach($professor_types as $id => $name)
-                  <option value="{{$id}}">{{$name}}</option>
-               @endforeach
-               </select>
-            </div>
-
-         </div>
-         <!-- End tipo de profesor -->
-
-         <!-- Start departamento academico -->
-         <div class="form-group professor-field" hidden>
-
-            <div class="col-sm-3">
-               <label class="control-label">Dep. Academico</label>
-            </div>
-
-            <div class="col-sm-9">
-               <select name="academic_department_id" class="form-control">
-                  <option value="0" selected>Seleccione el Dep. Academico del profesor</option>
-               @foreach($academic_departments as $academic_department)
-                  <option value="{{$academic_department->getId()}}">{{$academic_department->getName()}}</option>
-               @endforeach
-               </select>
-            </div>
-
-         </div>
-         <!-- End departamento academico -->
-
-         <div class="form-group">
-            <div class="col-sm-6">
-               <button class="btn btn-primary" name="registerButton" type="submit" value="register">Registrar</button>
-            </div>
-         </div>
-
+        <div class="col-sm-9">
+          <input type="file" name="photo" style="width: 100%;">
+        </div>
       </div>
+      <!-- End Foto -->
 
-   </form>
+      <!-- Start EAP -->
+      <div class="form-group student-field" hidden>
+        <div class="col-sm-3">
+          <label class="control-label">EAP</label>
+        </div>
 
+        <div class="col-sm-9">
+          <select name="school_id" class="form-control">
+            <option value="0" selected>Seleccione la EAP del alumno</option>
+            @foreach ($schools as $school)
+            <option value="{{ $school->id }}">{{ $school->name }}</option>
+            @endforeach
+          </select>
+        </div>
+      </div>
+      <!-- End EAP -->
+
+      <!-- Start Codigo -->
+      <div class="form-group student-field" hidden>
+        <div class="col-sm-3">
+          <label class="control-label">Código</label>
+        </div>
+
+        <div class="col-sm-9">
+          <input type="text" name="student_code" class="form-control" placeholder="Ingrese el código de alumno del estudiante" value="{{ old('student_code') }}">
+        </div>
+      </div>
+      <!-- End Codigo -->
+
+      <!-- Start año de ingreso -->
+      <div class="form-group student-field" hidden>
+        <div class="col-sm-3">
+          <label class="control-label">Año de ingreso</label>
+        </div>
+
+        <div class="col-sm-9">
+          <input type="text" name="year_of_entry" class="form-control" placeholder="Ingrese el año de ingreso del estudiante" value="{{ old('year_of_entry') }}">
+        </div>
+      </div>
+      <!-- End año de ingreso -->
+
+      <!-- Start tipo de profesor -->
+      <div class="form-group professor-field" hidden>
+        <div class="col-sm-3">
+          <label class="control-label">Tipo de profesor</label>
+        </div>
+
+        <div class="col-sm-9">
+          <select name="professor_type" class="form-control">
+            <option value="0" selected>Seleccione el tipo de profesor</option>
+
+            @foreach($professor_types as $id => $name)
+            <option value="{{ $id }}">{{ $name }}</option>
+            @endforeach
+          </select>
+        </div>
+      </div>
+      <!-- End tipo de profesor -->
+
+      <!-- Start departamento academico -->
+      <div class="form-group professor-field" hidden>
+        <div class="col-sm-3">
+          <label class="control-label">Dep. Academico</label>
+        </div>
+
+        <div class="col-sm-9">
+          <select name="academic_department_id" class="form-control">
+            <option value="0" selected>Seleccione el Dep. Academico del profesor</option>
+            @foreach($academic_departments as $academic_department)
+            <option value="{{$academic_department->id}}">{{$academic_department->name}}</option>
+            @endforeach
+          </select>
+        </div>
+      </div>
+      <!-- End departamento academico -->
+
+      <div class="form-group">
+        <div class="col-sm-6">
+          <button class="btn btn-primary" name="registerButton" type="submit" value="register">Registrar</button>
+        </div>
+      </div>
+    </div>
+  </form>
 </div>
 @endsection
