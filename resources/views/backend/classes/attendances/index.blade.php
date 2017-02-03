ed0268bc4c755175f07acdff1790f08932f6b276
Junior Claudio <juniorclaudiozavaleta@gmail.com>
2017-02-03 16:45:52 +0000

    list attendances

--- resources/views/backend/classes/attendances/index.blade.php
+++ resources/views/backend/classes/attendances/index.blade.php
@@ -2,35 +2,31 @@
 
 @section('content')
 
-<div class="row">
-  <div class="col-xs-12">
-    <h2>Asistencia de las Sesiones de {{ $clase->course_name }} / {{ $clase->type }}</h2>
-  </div>
+<div class="ui basic segment">
+  <h2>Asistencia de las Sesiones de {{ $clase->course_name }} / {{ $clase->type }}</h2>
 </div>
 
-<div class="row">
-  <table class="table">
-    <thead>
-      <th>#</th>
-      <th>Fecha</th>
-      <th>Estado de la sesion</th>
-      <th>Asistencia</th>
-      <th>Hora de registro</th>
-      <th>Acciones</th>
-    </thead>
-    <tbody>
-    @foreach ($attendances as $attendance)
-      <tr>
-        <td>{{ ($loop) }}</td>
-        <td>{{ $attendance->session_date }}</td>
-        <td>{{ $attendance->session_status }}</td>
-        <td>{{ $attendance->verified ? 'Asistio' : 'No Asistio'}}</td>
-        <td>{{ $attendance->created_at }}</td>
-        <td></td>
-      </tr>
-    @endforeach
-    </tbody>
-  </table>
-</div>
+<table class="ui table">
+  <thead>
+    <th>#</th>
+    <th>Fecha</th>
+    <th>Estado de la sesion</th>
+    <th>Asistencia</th>
+    <th>Hora de registro</th>
+    <th>Acciones</th>
+  </thead>
+  <tbody>
+  @foreach ($attendances as $attendance)
+    <tr>
+      <td>{{ ($loop->iteration) }}</td>
+      <td>{{ $attendance->session_date }}</td>
+      <td>{{ $attendance->session_status }}</td>
+      <td>{{ $attendance->verified ? 'Asistio' : 'No Asistio'}}</td>
+      <td>{{ $attendance->created_at }}</td>
+      <td></td>
+    </tr>
+  @endforeach
+  </tbody>
+</table>
 
 @endsection
