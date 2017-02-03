6072019822963e2d3f05b77f049e327a152b3ab2
Junior Claudio <juniorclaudiozavaleta@gmail.com>
2017-02-03 15:28:35 +0000

    index with semantic

--- resources/views/backend/index/professors.blade.php
+++ resources/views/backend/index/professors.blade.php
@@ -8,42 +8,37 @@
 </div>
 @endif
 
-<div class="row">
-  <div class="panel panel-primary">
-    <!-- Default panel contents -->
-    <div class="panel-heading">
-      <h2>Clases del ciclo</h2>
-    </div>
-    <!-- Table -->
-    <table class="table table-striped">
-      <thead>
-        <tr>
-          <th>Curso</th>
-          <th>Grupo</th>
-          <th>Tipo</th>
-          <th>Horario</th>
-          <th>Aula</th>
-          <th>Acciones</th>
-        </tr>
-      </thead>
-      <tbody>
-      @foreach ($classes as $class)
-        <tr>
-          <td>{{ $class->course_name  }}</td>
-          <td>{{ $class->group_number }}</td>
-          <td>{{ $class->type }}</td>
-          <td>{{ $class->schedule }}</td>
-          <td>{{ $class->classroom_name }}</td>
-          <td>
-            <a href="{{ route('classes.show', ['class' => $class->id]) }}" title="Ver datos de la clase"><i class="fa fa-eye fa-fw" aria-hidden="true"></i></a>
-            <a href="{{ route('classes.attendances.index', ['class' => $class->id]) }}" title="Ver registro de asistencias"><i class="fa fa-database fa-fw" aria-hidden="true"></i></a>
-            <a href="{{ route('classes.attendances.store', ['class' => $class->id]) }}" title="Registrar asistencia"><i class="fa fa-pencil fa-fw" aria-hidden="true"></i></a>
-          </td>
-        </tr>
-      @endforeach
-      </tbody>
-    </table>
-  </div>
+<div class="ui basic segment">
+  <h2>Clases del ciclo</h2>
 </div>
+
+<table class="ui compact table">
+  <thead>
+    <tr>
+      <th>Curso</th>
+      <th>Grupo</th>
+      <th>Tipo</th>
+      <th>Horario</th>
+      <th>Aula</th>
+      <th>Acciones</th>
+    </tr>
+  </thead>
+  <tbody>
+  @foreach ($classes as $class)
+    <tr>
+      <td>{{ $class->course_name  }}</td>
+      <td>{{ $class->group_number }}</td>
+      <td>{{ $class->type }}</td>
+      <td>{{ $class->schedule }}</td>
+      <td>{{ $class->classroom_name }}</td>
+      <td>
+        <a href="{{ route('classes.show', ['class' => $class->id]) }}" title="Ver datos de la clase"><i class="grey eye icon" aria-hidden="true"></i></a>
+        <a href="{{ route('classes.attendances.index', ['class' => $class->id]) }}" title="Ver registro de asistencias"><i class="teal database icon" aria-hidden="true"></i></a>
+        <a href="{{ route('classes.attendances.store', ['class' => $class->id]) }}" title="Registrar asistencia"><i class="blue pencil icon" aria-hidden="true"></i></a>
+      </td>
+    </tr>
+  @endforeach
+  </tbody>
+</table>
 
 @endsection
--- resources/views/backend/index/students.blade.php
+++ resources/views/backend/index/students.blade.php
@@ -2,41 +2,36 @@
 
 @section('content')
 
-<div class="row">
-   <div class="panel panel-primary">
-     <!-- Default panel contents -->
-      <div class="panel-heading">
-         <h2>Clases del ciclo</h2>
-      </div>
-     <!-- Table -->
-     <table class="table table-striped">
-       <thead>
-           <tr>
-               <th>Nombre</th>
-               <th>Grupo</th>
-               <th>Tipo</th>
-               <th>Horario</th>
-               <th>Aula</th>
-               <th>Acciones</th>
-           </tr>
-       </thead>
-       <tbody>
-           @foreach ($classes as $class)
-           <tr>
-              <td>{{ $class->professor_fullname }}</td>
-              <td>{{ $class->group_number }}</td>
-              <td>{{ $class->type }}</td>
-              <td>{{ $class->schedule }}</td>
-              <td>{{ $class->classroom_name }}</td>
-              <td>
-                 <a href="{{ route('classes.show', ['class' => $class->id]) }}" title="Ver datos de la clase"><i class="fa fa-eye fa-fw" aria-hidden="true"></i></a>
-                 <a href="{{ route('classes.attendances.index', ['class' => $class->id]) }}" title="Ver registro de asistencias"><i class="fa fa-database fa-fw" aria-hidden="true"></i></a>
-              </td>
-           </tr>
-           @endforeach
-       </tbody>
-     </table>
-   </div>
+<div class="ui basic segment">
+  <h2>Clases del ciclo</h2>
 </div>
+
+<table class="ui compact table">
+  <thead>
+    <tr>
+      <th>Nombre</th>
+      <th>Grupo</th>
+      <th>Tipo</th>
+      <th>Horario</th>
+      <th>Aula</th>
+      <th>Acciones</th>
+    </tr>
+  </thead>
+  <tbody>
+  @foreach ($classes as $class)
+    <tr>
+      <td>{{ $class->professor_fullname }}</td>
+      <td>{{ $class->group_number }}</td>
+      <td>{{ $class->type }}</td>
+      <td>{{ $class->schedule }}</td>
+      <td>{{ $class->classroom_name }}</td>
+      <td>
+         <a href="{{ route('classes.show', ['class' => $class->id]) }}" title="Ver datos de la clase"><i class="fa fa-eye fa-fw" aria-hidden="true"></i></a>
+         <a href="{{ route('classes.attendances.index', ['class' => $class->id]) }}" title="Ver registro de asistencias"><i class="fa fa-database fa-fw" aria-hidden="true"></i></a>
+      </td>
+    </tr>
+  @endforeach
+  </tbody>
+</table>
 
 @endsection
