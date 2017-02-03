65be127d994e80e9c7f38a3767ee4e5151812ff3
Junior Claudio <juniorclaudiozavaleta@gmail.com>
2017-02-03 14:58:13 +0000

    change look and feel to semantic

--- resources/views/backend/layout/base.blade.php
+++ resources/views/backend/layout/base.blade.php
@@ -2,87 +2,46 @@
 <html lang="en">
 
 <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+  <title>@yield('title')</title>
+  <link rel="stylesheet" type="text/css" href="{{ asset('/semantic.min.css') }}">
+  <script src="{{ asset('/jquery-3.1.1.min.js') }}"></script>
+  <script src="{{ asset('/semantic.min.js') }}"></script>
 
-    <title>SB Admin 2 - Bootstrap Admin Theme</title>
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/font-awesome.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/sb-admin-2.css') }}">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-    <!-- Scripts -->
-    <script src="{{ url('js/jquery-1.11.3.min.js') }}"></script>
-    <script src="{{ url('js/bootstrap.min.js') }}"></script>
-    <script src="{{ url('js/metisMenu.min.js') }}"></script>
-    <script src="{{ url('js/sb-admin-2.js') }}"></script>
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
+  @stack('head')
 </head>
 
 <body>
-    <div id="wrapper">
-        <!-- Navigation -->
-        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.html">SB Admin v2.0</a>
-            </div>
-            <!-- /.navbar-header -->
+  <div class="ui top attached menu">
+    <a class="item" id="menu-button">
+      <i class="sidebar icon"></i>
+      Menu
+    </a>
+  </div>
 
-            <ul class="nav navbar-top-links navbar-right">
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                        <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
-                    </a>
-                    <ul class="dropdown-menu dropdown-user">
-                        <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
-                        </li>
-                        <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
-                        </li>
-                        <li class="divider"></li>
-                        <li><a href="{{ route('auth.logout') }}"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
-                        </li>
-                    </ul>
-                    <!-- /.dropdown-user -->
-                </li>
-                <!-- /.dropdown -->
-            </ul>
-            <!-- /.navbar-top-links -->
+  <div class="ui bottom attached segment pushable" id="content">
+    @include('backend.layout.sidebar')
 
-            @include('backend.layout.sidebar')
-        </nav>
-
-        <div id="page-wrapper">
-            @yield('content')
-        </div>
-        <!-- /#page-wrapper -->
-
+    <div class="pusher">
+      <div class="ui container">
+        @yield('content')
+      </div>
     </div>
-    <!-- /#wrapper -->
-    <script type="text/javascript">
-       var base_url = '{{ url("/")}}';
-    </script>
-</body>
+  </div>
 
+  <!-- Scripts -->
+  <script>
+  $('#menu-button').click(function(){
+    $('.ui.sidebar').sidebar({
+      context: $('.bottom.segment')
+    }).sidebar('toggle');
+  });
+  </script>
+  @stack('scripts')
+</body>
 </html>
--- resources/views/backend/layout/sidebar.blade.php
+++ resources/views/backend/layout/sidebar.blade.php
@@ -1,20 +1,6 @@
-<div class="navbar-default sidebar" role="navigation">
-  <div class="sidebar-nav navbar-collapse">
-    <ul class="nav" id="side-menu">
-      <li>
-         <a href="{{ route('index') }}"><i class="fa fa-dashboard fa-fw"></i>Index</a>
-      </li>
-      <li>
-         <a href="{{ route('users.index') }}"><i class="fa fa-user fa-fw"></i>Usuarios</a>
-      </li>
-      <li>
-         <a href="{{ route('facultades.index') }}"><i class="fa fa-building-o fa-fw"></i>Facultades</a>
-      </li>
-      <li>
-         <a href="{{ route('eaps.index') }}"><i class="fa fa-graduation-cap fa-fw"></i>EAPs</a>
-      </li>
-    </ul>
-  </div>
-  <!-- /.sidebar-collapse -->
+<div class="ui sidebar vertical tiny labeled icon menu" id="sidebar">
+ <a class="item" href="{{ route('index') }}"><i class="fa fa-dashboard fa-fw"></i>Index</a>
+ <a class="item" href="{{ route('users.index') }}"><i class="fa fa-user fa-fw"></i>Usuarios</a>
+ <a class="item" href="{{ route('facultades.index') }}"><i class="fa fa-building-o fa-fw"></i>Facultades</a>
+ <a class="item" href="{{ route('eaps.index') }}"><i class="fa fa-graduation-cap fa-fw"></i>EAPs</a>
 </div>
-<!-- /.navbar-static-side -->
